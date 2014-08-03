e78750aab22236ee66b73fcfddcd9e3e07df8e47
Matthew Hootman <mhootman1980@gmail.com>
2014-08-03 03:29:36 -0500

    updated stat and team models

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,3 +1,24 @@
 class Stat
   include Mongoid::Document
+
+  field :year, type: Integer
+  field :wins, type: Integer
+  field :losses, type: Integer
+  field :fg_pct, type: Float  # field goal %
+  field :ft_pct, type: Float  # free throw %
+  field :tp_pct, type: Float  # 3-pt %
+  field :ppp, type: Float     # points per possession
+  field :ppg, type: Float     # points per game
+  field :trpg, type: Float    # total rebounds per game
+  field :orpg, type: Float    # offensive rebounds per game
+  field :drpg, type: Float    # defensive rebounds per game
+  field :apg, type: Float     # assists per game
+  field :spg, type: Float     # steals per game
+  field :tpg, type: Float     # turnovers per game
+  field :bpg, type: Float     # blocks per game
+  field :fpg, type: Float     # fouls per game
+  field :rpi, type: Float     # RPI
+  field :sos, type: Float     # SOS
+
+  belongs_to :team
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,9 @@
 class Team
   include Mongoid::Document
+
+  field :name, type: String
+  field :mascot, type: String
+  field :location, type: String
+
+  has_many :stats
 end
